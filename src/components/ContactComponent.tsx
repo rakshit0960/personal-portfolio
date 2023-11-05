8d1a4b9f372dbc6a379e39213405f3e1c21c2e96
Rakshit Mehta <rakshit0960@gmail.com>
2023-11-05 17:03:05 +0530

    added hover effect

--- src/components/ContactComponent.tsx
+++ src/components/ContactComponent.tsx
@@ -7,7 +7,7 @@ interface Prop {
 export default function ContactComponent({icon, info}: Prop) {
   return (
     
-    <div className="px-6 py-6 border-2 rounded-2xl">
+    <div className="px-6 py-6 border-2 rounded-2xl cursor-pointer hover:border-gray-600 ">
       <code className="flex items-center text-2xl">
         {`<`}
             <div className="pl-3">{icon}</div> 
--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -12,7 +12,7 @@ export default function ContactForm() {
               name="firstName"
               required={true}
               placeholder="Name"
-              className="border-2 p-2 font-semibold w-full"
+              className="border-2 p-2 font-semibold w-full focus:outline-none focus:ring-1 focus:ring-black"
             />
           </label>
           <label>
@@ -22,7 +22,7 @@ export default function ContactForm() {
               name="lastName"
               required={true}
               placeholder="Name"
-              className="border-2 p-2 font-semibold w-full"
+              className="border-2 p-2 font-semibold w-full focus:outline-none focus:ring-1 focus:ring-black"
             />
           </label>
         </div>
@@ -35,7 +35,7 @@ export default function ContactForm() {
             required={true}
             placeholder="Email"
             autoComplete="email"
-            className="border-2 p-2 font-semibold w-full"
+            className="border-2 p-2 font-semibold w-full focus:outline-none focus:ring-1 focus:ring-black"
           />
         </label>
         <label>
@@ -46,10 +46,10 @@ export default function ContactForm() {
             placeholder="Message"
             rows={6}
             cols={40}
-            className="border-2 resize-none p-2 w-full font-semibold"
+            className="border-2 resize-none p-2 w-full font-semibold focus:outline-none focus:ring-1 focus:ring-black"
           ></textarea>
         </label>
-        <button type="submit" className="border-2 p-2 mt-4 rounded-xl">
+        <button type="submit" className="border-2 p-2 mt-4 rounded-xl hover:border-gray-500 focus:outline-none focus:ring-1 focus:ring-black">
           Send
         </button>
       </form>
