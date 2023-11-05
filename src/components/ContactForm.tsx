877bb495ad581768dabdaa7c6e86c82ff9ed0966
Rakshit Mehta <rakshit0960@gmail.com>
2023-11-05 22:37:06 +0530

    send basic messages using emailJS

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -1,9 +1,35 @@
 import SectionWrapper from "./SectionWrapper";
+import { FormEventHandler, useRef } from "react";
+import emailjs from "@emailjs/browser";
 
 export default function ContactForm() {
+  const formRef = useRef<HTMLFormElement>(null);
+
+  const sendEmail: FormEventHandler = async (e) => {
+    e.preventDefault();
+    if (formRef.current == null) return;
+
+    try {
+      await emailjs.sendForm(
+          import.meta.env.VITE_EMAILJS_SERVICE_ID,
+          import.meta.env.VITE_EMAILJS_TEMPLATE_ID,
+          formRef.current,
+          import.meta.env.VITE_EMAILJS_PUBLIC_KEY
+        )
+    } catch(error) {
+        console.log('error sending message ', error);
+    }
+
+
+  };
+
   return (
     <SectionWrapper name="Form">
-      <form className="grid grid-flow-row gap-4">
+      <form
+        onSubmit={sendEmail}
+        ref={formRef}
+        className="grid grid-flow-row gap-4"
+      >
         <div className="grid grid-flow-col gap-4">
           <label>
             First Name <br />
@@ -49,7 +75,10 @@ export default function ContactForm() {
             className="border-2 resize-none p-2 w-full font-semibold focus:outline-none focus:ring-1 focus:ring-black"
           ></textarea>
         </label>
-        <button type="submit" className="border-2 p-2 mt-4 rounded-xl hover:border-gray-500 focus:outline-none focus:ring-1 focus:ring-black">
+        <button
+          type="submit"
+          className="border-2 p-2 mt-4 rounded-xl hover:border-gray-500 focus:outline-none focus:ring-1 focus:ring-black"
+        >
           Send
         </button>
       </form>
