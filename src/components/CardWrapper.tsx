cc353af8bf0815b7add0a2c7113233a8a832d6f8
Rakshit Mehta <rakshit0960@gmail.com>
2023-11-08 19:46:42 +0530

    updated projects section

--- src/components/CardWrapper.tsx
+++ src/components/CardWrapper.tsx
@@ -1,6 +1,6 @@
 export default function CardWrapper({children, name}: {children: string | JSX.Element | JSX.Element[], name: string}) {
   return (
-    <div className="border-2 p-6">
+    <div className="border-2 p-6 rounded-lg">
         <code className="text-2xl">{`<${name}>`}</code>
             <div className="mx-6 my-4">{children}</div>
         <code className="text-2xl">{`</${name}>`}</code>
--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -1,19 +1,27 @@
+import { FaGithub } from "react-icons/fa6";
 import { IProject } from "../data/projectObjectInterface";
 import CardWrapper from "./CardWrapper";
+import { BsLink45Deg } from "react-icons/bs";
 
 export default function ProjectCard({ project }: { project: IProject }) {
   return (
     <CardWrapper name={'Project'}>
-        <div className="aspect-16/11 w-full border-2">
-          <img src={project.preview}/>
+        <div className="aspect-video w-full border-2 rounded-lg">
+          <img className="object-cover h-full w-full" src={project.preview}/>
         </div>
 
         <p className="text-2xl leading-relaxed">{project.name}</p>
         <p className="text-lg">{project.description}</p>
 
         <div className="flex justify-between my-5">
-            <a className="border-2 px-4 py-2" href={project.repositoryLink}>GitHub</a>
-            <a className="border-2 px-4 py-2" href={project.HostedLink}>Demo</a>
+            <a className="border-2 px-10 py-2 rounded-lg flex items-center bg-gray-700 text-white" href={project.repositoryLink} target="_blank">
+              <FaGithub size='1.3em'color={'#fff'}/>
+              <div className="px-2">GitHub</div>
+            </a>
+            <a className="border-2 px-10 py-2 rounded-lg flex items-center" href={project.HostedLink} target="_blank">
+              <BsLink45Deg size='1.7em'/>
+              <div className="px-2">Demo</div>
+            </a>
         </div>
     </CardWrapper>
   );
