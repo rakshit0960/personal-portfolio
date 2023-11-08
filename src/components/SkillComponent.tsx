7c41caead3c94ca528556356ce6b0c82cd5fcb47
Rakshit Mehta <rakshit0960@gmail.com>
2023-11-08 19:02:09 +0530

    updated skill components gap

--- src/components/SkillComponent.tsx
+++ src/components/SkillComponent.tsx
@@ -4,8 +4,14 @@ import SingleComponentWrapper from "./SingleComponentWrapper";
 export default function SkillComponent({ skill }: { skill: ISkill }) {
   return (
     <SingleComponentWrapper name={skill.name} fontSize="1.6rem">
-      <div className="border-2 h-3">
-        <div className="bg-gray-600 h-full" style={{width: `${skill.level}%`}}></div>
+      <div className="flex items-center">
+        <div className="border-2 h-3 w-full">
+          <div
+            className="bg-gray-600 h-full"
+            style={{ width: `${skill.level}%` }}
+          ></div>
+        </div>
+        <div className="ml-2 text-xl">{skill.level}%</div>
       </div>
     </SingleComponentWrapper>
   );
