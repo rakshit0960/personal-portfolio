3ddb04e549c5fcff537ad7d6a4265162a59f4258
Rakshit Mehta <rakshit0960@gmail.com>
2023-11-08 18:52:53 +0530

    updated skills components

--- src/components/SkillComponent.tsx
+++ src/components/SkillComponent.tsx
@@ -3,9 +3,9 @@ import SingleComponentWrapper from "./SingleComponentWrapper";
 
 export default function SkillComponent({ skill }: { skill: ISkill }) {
   return (
-    <SingleComponentWrapper name={skill.name}>
-      <div className="border-2 h-4">
-        <div className="bg-black h-full" style={{width: `${skill.level}%`}}></div>
+    <SingleComponentWrapper name={skill.name} fontSize="1.6rem">
+      <div className="border-2 h-3">
+        <div className="bg-gray-600 h-full" style={{width: `${skill.level}%`}}></div>
       </div>
     </SingleComponentWrapper>
   );
--- src/components/SkillsSection.tsx
+++ src/components/SkillsSection.tsx
@@ -9,7 +9,7 @@ const skillsList: ISkill[] = data.skills;
 export default function SkillsSection() {
   return (
     <SectionWrapper name='Tech Stack'>
-      <div className="grid grid-cols-2 gap-4">
+      <div className="grid grid-cols-2 gap-y-12 gap-x-8">
         {skillsList.map(skill => <SkillComponent skill={skill} key={skill.id}/>)}
       </div>
     </SectionWrapper>
