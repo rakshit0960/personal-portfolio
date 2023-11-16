8b06f14c7782bed91850f080885a687406f9a465
Rakshit Mehta <rakshit0960@gmail.com>
2023-11-16 18:09:51 +0530

    changed home title

--- src/components/HomeSection.tsx
+++ src/components/HomeSection.tsx
@@ -2,7 +2,7 @@ export default function HomeSection() {
   return (
     <div className="flex flex-col lg:flex-row justify-between items-center lg:px-36 lg:pb-20 px-4 pb-6">
       <div className="order-last lg:order-first">
-        <div className="text-3xl lg:text-5xl">Full Stack Developer</div>
+        <div className="text-3xl lg:text-5xl">Developer</div>
         <div className="text-base lg:text-xl pt-2 pl-10">
           Hi, i am Rakshit Mehta, A Computer science student, and <br />  developer based in India
         </div>
